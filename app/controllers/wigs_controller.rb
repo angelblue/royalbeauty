f3013b14655dafa92c5bc6539379c5aebee5f370
B.Y <contact@royalbeautychicago.com>
2012-11-13 11:37:29 -0600

    first commit

---
+++ app/controllers/wigs_controller.rb
@@ -0,0 +1,4 @@
+class WigsController < ApplicationController
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Royal::Application.config.secret_token = '30db3caf5277fd0e0a47a17b7108ae13889230e4b067af46252fcfe27b6ea1e423641efa3bdef8e4fdbc77ef239e28d986a0a62b832b6513997f4a2790e1702c'
